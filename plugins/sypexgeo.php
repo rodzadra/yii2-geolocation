110233847fa04cc717b715c993b229d80dc500a7
rodzadra <rodzadra@gmail.com>
2015-08-19 17:44:43 -0300

    Add SypexGeo plugin.

---
+++ plugins/sypexgeo.php
@@ -0,0 +1,10 @@
+<?php
+
+$plugin = [
+            'plugin_url'                => 'http://api.sypexgeo.net/{{accepted_formats}}/{{ip}}',
+            'accepted_formats'          => ['json', 'xml'],
+            'default_accepted_format'   => 'json',
+    ];
+
+
+
